9d9a7334151aca9b9dd7a6590227864b58c6ca2c
Bishal Peter Dores <bishalpd@gmail.com>
2020-01-30 13:09:17 +0600

    Updates

---
+++ precache-manifest.e710f86470c8ef355b5f721fe502a1b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0558d7a791616c73661c",
+    "url": "/lyricFinder/static/js/runtime~main.6a6d2297.js"
+  },
+  {
+    "revision": "e2e0c48bc3751853e8f7",
+    "url": "/lyricFinder/static/js/main.30f43748.chunk.js"
+  },
+  {
+    "revision": "5e28292d3e365abf5614",
+    "url": "/lyricFinder/static/js/2.88ff87d7.chunk.js"
+  },
+  {
+    "revision": "e2e0c48bc3751853e8f7",
+    "url": "/lyricFinder/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "81e6c76f37f5ad1166cec426918b553b",
+    "url": "/lyricFinder/index.html"
+  }
+];
